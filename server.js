7f85ee6943d9a9b55ff7d175ea7de93fa99d414a
Monir Hossain Rabby <monirhrabby.personal@gmail.com>
2025-01-28 21:40:44 +0600

    fsdf

--- server.js
+++ server.js
@@ -1,26 +1,26 @@
 const express = require("express");
-const serverless = require("serverless-http");
+const bodyParser = require("body-parser");
 
 const app = express();
+const PORT = 5000;
 
 // Middleware to parse JSON request bodies
-app.use(express.json());
+app.use(bodyParser.json());
 
 // Simple /login endpoint
 app.post("/login", (req, res) => {
-  const user = {
-    email: "test@gmail.com",
-    password: "123456",
-  };
-  const { username, password } = req.body;
+  const { email, password } = req.body;
 
-  if (!username || !password) {
+  if (!email || !password) {
     return res
       .status(400)
       .json({ message: "Username and password are required." });
   }
 
-  if (username === user["email"] && password === user["password"]) {
+  console.log(req.body);
+
+  // Dummy login logic
+  if (email === "test@gmail.com" && password === "123456") {
     return res
       .status(200)
       .json({ message: "Login successful!", token: "dummy-token" });
@@ -29,6 +29,7 @@ app.post("/login", (req, res) => {
   }
 });
 
-// Export the app wrapped in serverless-http
-module.exports = app;
-module.exports.handler = serverless(app);
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
